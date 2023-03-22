b878c774720d0196f24deb4d60088c1cc3c3bb0a
MalachyS <S00221188@mail.itsligo.ie>
2023-03-22 17:32:36 +0000

    s

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,31 @@
 import {HttpClientModule} from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { OmdbApiService } from './services/omdb-api.service';
+import { SearchtitleComponent } from './components/searchtitle/searchtitle.component';
+import { AboutComponent } from './components/about/about.component';
+import { SearchComponent } from './componets/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchtitleComponent,
+    AboutComponent,
+    SearchComponent
   ],
+  const routes:Routes= [
+    {path:'', component:SearchtitleComponent},
+    {path:'about', component:AboutComponent},
+    {path:'search', component:SearchComponent}
+    ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [OmdbApiService],
   bootstrap: [AppComponent]
